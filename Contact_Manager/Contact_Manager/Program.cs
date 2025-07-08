63647e2b36163a62ed1f635b5fc52d02474d9d58
Velmurugan Lakshmanamoorthy <velmurugan.kl@solitontech.com>
2025-07-08 18:26:53 +0530

    July_end commit

--- Contact_Manager/Contact_Manager/Program.cs
+++ Contact_Manager/Contact_Manager/Program.cs
@@ -1,2 +1,119 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using System.Net.Mail;
+
+SortedList<string, Info> contactInfo = new SortedList<string, Info>();
+List<Info> list = new List<Info>();
+
+string userInput;
+do
+{
+    Console.WriteLine("Enter Your choice!!!");
+    Console.WriteLine("S - To View the contacts");
+    Console.WriteLine("A - To Add contact");
+    Console.WriteLine("D - To Delete a contact");
+    Console.WriteLine("E - To Exit");
+    userInput = Console.ReadLine().ToUpper();
+    HandleChoice(userInput);
+}
+while (userInput != "E");
+
+void HandleChoice(string choice)
+{
+    switch (choice)
+    {
+        case "S":
+            ViewContacts();
+            break;
+        case "A":
+            AddContact();
+            break;
+    }
+}
+
+void ViewContacts()
+{
+    string choice;
+
+    Console.WriteLine("A - To view all contacts");
+    Console.WriteLine("N - To view contacts by name");
+    Console.WriteLine("PH - To view contacts by number");
+    Console.WriteLine("E - To go back to Main Menu");
+    choice = Console.ReadLine().ToUpper();
+    switch (choice)
+    {
+        case "A":
+            DisplayAllContacts();
+            break;
+        case "N":
+            SearchByNumber();
+            break;
+        case "PH":
+            SearchByPhone();
+            break;
+        case "E":
+            return;
+    }
+}
+
+void SearchByPhone()
+{
+    throw new NotImplementedException();
+}
+
+void SearchByNumber()
+{
+    throw new NotImplementedException();
+}
+
+void DisplayAllContacts()
+{
+    throw new NotImplementedException();
+}
+
+void AddContact()
+{
+    string name;
+    string number;
+    uint phone;
+    string mail;
+    String notes;
+
+    Console.WriteLine("Enter the name");
+    name = Console.ReadLine();
+  
+    do
+    {
+        Console.WriteLine("Enter the number");
+        number = Console.ReadLine();
+    }
+    while (!uint.TryParse(number, out uint result));
+    phone = uint.Parse(number);
+    Console.WriteLine("Enter the mail");
+    mail = Console.ReadLine();
+    Console.WriteLine("Write any notes about the contact");
+    notes = Console.ReadLine();
+
+    Info obj = new Info(name, mail, phone, notes);
+
+
+}
+
+
+class Info
+{
+    public string name;
+    public string email;
+    public uint phone;
+    public string notes;
+
+    public Info(string name, string email, uint phone, string notes)
+    {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.notes = notes;
+    }
+}
---
+++ Contact_Manager/Testing/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+SortedList<string, Info> contactInfo = new SortedList<string, Info>();
+Console.WriteLine("Hello User");
+string userInput;
+do
+{
+    Console.WriteLine("Enter Your choice!!!");
+    Console.WriteLine("1 - To View the contacts");
+    Console.WriteLine("2 - To Add contact");
+    Console.WriteLine("3 - To Delete a contact");
+    Console.WriteLine("4 - To Exit");
+    userInput = Console.ReadLine();
+    HandleChoice(int.Parse(userInput));
+}
+while (userInput != "4");
+
+void HandleChoice(int choice)
+{
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Display");
+            DisplayContactInfo();
+            break;
+        case 2:
+            AddContact();
+            break;
+    }
+}
+
+void DisplayContactInfo()
+{
+    for(int i = 0; i < contactInfo.Count; i++)
+    {
+        Console.WriteLine($"{contactInfo.GetKeyAtIndex(i)} --- {contactInfo.GetValueAtIndex(i)}" +
+            $" -- number of the person {contactInfo.GetValueAtIndex(i).phone}");
+    }
+}
+void AddContact()
+{
+    Console.WriteLine("Enter the Phone number");
+    string phone = Console.ReadLine();
+    Console.WriteLine("Enter the Name");
+    string name = Console.ReadLine();
+    Console.WriteLine("Enter Email");
+    string mail = Console.ReadLine();
+    Console.WriteLine("Enter notes about the person");
+    string notes = Console.ReadLine();
+    Info obj = new Info(name,mail,phone,notes);
+    contactInfo.Add(name,obj);
+}
+
+public class Info
+{
+    public string name;
+    public string email;
+    public string phone;
+    public string notes;
+
+    public Info(string name, string email, string phone, string notes)
+    {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.notes = notes;
+    }
+}
